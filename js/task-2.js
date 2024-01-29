6d9b8ee2fe8819b6776b15aae55cef3462d8ce45
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2024-01-29 19:52:30 +0100

    Update task2

--- js/task-2.js
+++ js/task-2.js
@@ -56,6 +56,7 @@ gallery.style.listStyle = 'none';
 gallery.style.display = 'flex';
 gallery.style.flexWrap = 'wrap';
 gallery.style.gap = '48px 24px';
+gallery.style.justifyContent = 'space-between';
 
 
 
